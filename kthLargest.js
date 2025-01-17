865292d9d84cd5dcabd72c78548ae45b44ce8c64
Saharul Sifat <126339980+MernSpace@users.noreply.github.com>
2025-01-17 19:06:21 +0600

    Kth largest number find done and margesortedarray done

--- kthLargest.js
+++ kthLargest.js
@@ -1,6 +1,16 @@
-function kthLargest(arra,n){
-
+function kthLargest(arr,n){
+    const minHeap = [];
+  
+    for (let num of arr) {
+      minHeap.push(num);
+      minHeap.sort((a, b) => a - b);
+      if (minHeap.length > n) {
+        minHeap.shift();
+      }
+    }
+    
+    return minHeap[0]; 
 }
 
 
-kthLargest([1, 23, 12, 9, 30, 2, 50], 3); // 23
+console.log(kthLargest([1, 23, 12, 9, 30, 2, 50], 3))
--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,11 +1,31 @@
 function mergeSortedArrays(arra1,arra2){
-    for(var i = 0; i < arra1.length; i++){
-        arra2.push(arra1[i]);
-    }
+    let i = 0, j = 0;
+    const mergedArray = [];
+  
 
+    while (i < arra1.length && j < arra2.length) {
+      if (arra1[i] < arra2[j]) {
+        mergedArray.push(arra1[i]);
+        i++;
+      } else {
+        mergedArray.push(arra2[j]);
+        j++;
+      }
+    }
+    while (i < arra1.length) {
+        mergedArray.push(arra1[i]);
+        i++;
+      }
+
+      while (j < arra2.length) {
+        mergedArray.push(arra2[j]);
+        j++;
+      }
+    
+      return mergedArray;
 
 }
 
 
 
-mergeSortedArrays([3,4,31],[4,6,30]); 
+console.log(mergeSortedArrays([3,4,31],[4,6,30,60,70,80,90])) 
